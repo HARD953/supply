9b5afb7485aaad7fb4361d9d9138ee8906b270d0
issa <lanfiatech2022@gmail.com>
2025-03-15 18:34:57 +0100

    add new urls

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Category, Product, ProductFormat, Order, OrderItem
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .serializers import (
     CategorySerializer,
     ProductSerializer,
@@ -13,9 +15,46 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+# class ProductViewSet(viewsets.ModelViewSet):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]  # Accès réservé aux utilisateurs authentifiés
+
+    # Vue par défaut : tous les produits
+    def get_queryset(self):
+        return Product.objects.all()
+
+    # Vue pour les produits des Fabricants
+    @action(detail=False, methods=['get'], url_path='fabricant')
+    def fabricant_products(self, request):
+        queryset = Product.objects.filter(supplier__user_type='Fabricant')
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    # Vue pour les produits des Grossistes
+    @action(detail=False, methods=['get'], url_path='grossiste')
+    def grossiste_products(self, request):
+        queryset = Product.objects.filter(supplier__user_type='Grossiste')
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    # Vue pour les produits des Semi-Grossistes
+    @action(detail=False, methods=['get'], url_path='semi-grossiste')
+    def semi_grossiste_products(self, request):
+        queryset = Product.objects.filter(supplier__user_type='Semi-Grossiste')
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    # Vue pour les produits des Détaillants
+    @action(detail=False, methods=['get'], url_path='detaillant')
+    def detaillant_products(self, request):
+        queryset = Product.objects.filter(supplier__user_type='Détaillant')
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 class ProductFormatViewSet(viewsets.ModelViewSet):
     queryset = ProductFormat.objects.all()
