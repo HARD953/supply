e0cd6736b9260eb0966776bc9fef4e5fdaa1498f
issa <lanfiatech2022@gmail.com>
2025-08-03 18:10:57 +0200

    add nex

--- superM/settings.py
+++ superM/settings.py
@@ -144,7 +144,7 @@ REST_FRAMEWORK = {
     #     'rest_framework.permissions.IsAuthenticated',
     # ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        #'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',  # Ajoutez cette ligne
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
